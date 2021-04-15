a59890a0b38eaaa0e451c4094408329c220d80c7
marijanbrvar <marijan.brvar@gmail.com>
2021-04-15 11:21:00 +0200

    Add user association

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_085940) do
+ActiveRecord::Schema.define(version: 2021_04_15_090958) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_attendances_on_event_id"
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.string "date"
     t.string "location"
-    t.string "creator"
+    t.integer "creator"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -28,4 +37,6 @@ ActiveRecord::Schema.define(version: 2021_04_15_085940) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "attendances", "events"
+  add_foreign_key "attendances", "users"
 end
