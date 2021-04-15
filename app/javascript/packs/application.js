76f294d14ac8a9f51b5ff08b8ad55b9989d8e80e
marijanbrvar <marijan.brvar@gmail.com>
2021-04-15 10:08:50 +0200

    Add bootstrap 5 beta

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap/dis/js/bootstrap"
+import "bootstrap/dis/css/bootstrap"
 
 Rails.start()
 Turbolinks.start()
