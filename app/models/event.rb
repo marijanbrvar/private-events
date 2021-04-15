de7935d9f9ab6876b28e1d703954bcaea7c07614
marijanbrvar <marijan.brvar@gmail.com>
2021-04-15 11:31:25 +0200

    Add event association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ApplicationRecord
+    belongs_to :creator, class_name: 'User', foreign_key: :creator
+    has_many :attendances
+    has_many :attendees, through: :attendances, source: :user
+
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :date, presence: true
+    validates :location, presence: true
 end
