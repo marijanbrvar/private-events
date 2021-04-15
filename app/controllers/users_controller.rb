7265d337614d5a29f21cbcb6b183761247f679db
Zil Norvilis <zilasino27@gmail.com>
2021-04-15 10:57:38 +0200

    Add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
+  include UsersHelper
+
   def new
+    if session[:current_user]
+      redirect_to events_path
+    else
+      @user = User.new
+    end
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      current_user(@user.id)
+    else
+      render :new
+    end
   end
 
   def show
   end
+
 end
